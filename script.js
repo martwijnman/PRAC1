0b05ebde4f2a6b4a0e178f2a6f19e9cb4bae330a
martwijnman <153086626+martwijnman@users.noreply.github.com>
2024-09-04 10:16:39 +0200

    Update script.js
    I put extra code, to make an animation.

--- script.js
+++ script.js
@@ -39,11 +39,30 @@ window.addEventListener('DOMContentLoaded', function(){
         const targetElement = document.getElementById(targetId);
 
         if (targetElement) {
-            targetElement.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            })
+            slowScrollTo(targetElement);
         }
     })
 })
+function slowScrollTo(element){
+    const targetPosition = element.getBoundingClientRect().top + window.pageYOffset;
+    const distance =  targetPosition - startPosition;
+    const duration = 1500;
+    let startTime = null;
+
+    function animation(currentTime){
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+    requestAnimationFrame(animation);
+}
 })
